c41a8514e70f8911b8e9424b92223cead5f3f688
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-05-25 22:29:34 +0300

    burger in gallery header

--- src/pages/Gallery/components/GalleryHeader/GalleryHeader.tsx
+++ src/pages/Gallery/components/GalleryHeader/GalleryHeader.tsx
@@ -2,7 +2,7 @@ import PageHeader from "@/components/PageHeader/PageHeader";
 import SearchInput from "@/components/UI/SearchInput/SearchInput";
 import styles from "./GalleryHeader.module.scss";
 import { useState } from "react";
-import Button from './../../../../components/UI/Button/Button';
+import Button from "./../../../../components/UI/Button/Button";
 import PlusIcon from "@/components/UI/icons/PlusIcon";
 import { Link } from "react-router-dom";
 import PageLink from "@/components/UI/PageLink/PageLink";
@@ -10,11 +10,10 @@ import PageLink from "@/components/UI/PageLink/PageLink";
 const GalleryHeader = () => {
    const [searchValue, setSearchValue] = useState<string>("");
 
-   const handleSearch = () => {
-   };
+   const handleSearch = () => {};
 
    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSearchValue(event.target.value)
+      setSearchValue(event.target.value);
    };
 
    return (
@@ -27,11 +26,22 @@ const GalleryHeader = () => {
             />
          }
       >
-         <Link to="">Ваша галерея</Link>
-         <PageLink to="" className={styles.link}>Свежие</PageLink>
-         <Button icon={<PlusIcon/>} className={styles.link}>
-            Добавить
-         </Button>
+         <div className={styles.content}>
+            <div className={styles.links}>
+               <Link to="">Ваша галерея</Link>
+               <PageLink to="" className={styles.link}>
+                  Свежие
+               </PageLink>
+               <Button icon={<PlusIcon />} className={styles.link}>
+                  Добавить
+               </Button>
+            </div>
+            <Button className={styles.burger}>
+               <div className={styles.burgerLine}></div>
+               <div className={styles.burgerLine}></div>
+               <div className={styles.burgerLine}></div>
+            </Button>
+         </div>
       </PageHeader>
    );
 };

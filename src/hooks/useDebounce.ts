aa57b2cb69a0b183b7b784e0a6f6981bc4ee92a2
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-07-09 00:30:02 +0300

    NotFound page

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,12 @@
 import { useCallback, useRef } from 'react';
 
-export const useDebounce = (callback: (...args: any[]) => any, delay: number) => {
+/**
+ * Возвращает callback, который выполнится только после заданного delay
+ * @param callback - функция для создания debouncedCallback
+ * @param delay - задержка
+ * @returns 
+ */
+export const useDebounce = (callback: (...args: any[]) => any, delay: number): (...args: any[]) => any => {
   const timeoutRef = useRef<null | ReturnType<typeof setTimeout>>(null);
 
   const debouncedCallback = useCallback((...args: any[]) => {
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import styles from './NotFound.module.scss'
+
+const index = () => {
+  return (
+    <div className={styles.content}>
+      {'Здесь ничего нет :('}
+    </div>
+  )
+}
+
+export default index
